764385471428ebe4df79b482423eb86a99825cec
carloscubas <carlos.cubas@gmail.com>
2024-04-03 19:54:26 -0300

    add virtual host

--- async/cooking/src/main/java/com/restaurant/cooking/config/RabbitMQConfig.java
+++ async/cooking/src/main/java/com/restaurant/cooking/config/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package com.restaurant.cooking.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
